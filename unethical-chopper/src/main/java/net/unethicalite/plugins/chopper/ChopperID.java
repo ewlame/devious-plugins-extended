3f68ac985488f044aedfa0e3329f265d0b32f4ef
unknown <mattschubert@gmail.com>
2023-08-15 18:13:31 -0400

    modified:   unethical-chopper/src/main/java/net/unethicalite/plugins/chopper/ChopperID.java

--- unethical-chopper/src/main/java/net/unethicalite/plugins/chopper/ChopperID.java
+++ unethical-chopper/src/main/java/net/unethicalite/plugins/chopper/ChopperID.java
@@ -13,7 +13,24 @@ public interface ChopperID
     int DROPPINGS = 47495;
     int GREEN_LEAVES = 47494;
     int SPLINTERED_BARK = 47499;
+    int[] saplingIngredients = new int[] {GREEN_ROOTS, ROTTING_LEAVES, WILD_MUSHROOMS,
+            WILD_MUSHROOMS_47498, WILD_MUSHROOMS_47497,DROPPINGS,GREEN_LEAVES,SPLINTERED_BARK};
     int STRUGGLING_SAPLING = 47487;
     int THRIVING_SAPLING = 47489;
+    int STRANGE_BUSH_LILAC = 12133;
+    int FLOWERING_BUSH_PINK = 12134;
+    int STRANGE_BUSH_PINK = 12135;
+    int FLOWERING_BUSH_RED = 12141;
+    int STRANGE_BUSH_RED = 12142;
+    int FLOWERING_BUSH_ORANGE = 12143;
+    int STRANGE_BUSH_ORANGE = 12144;
+    int FLOWERING_BUSH_YELLOW = 12145;
+    public static final int STRANGE_BUSH_YELLOW = 12146;
+    public static final int FLOWERING_BUSH_WHITE = 12147;
+    public static final int STRANGE_BUSH_WHITE = 12148;
+    public static final int FLOWERING_BUSH_GREEN = 12149;
+    public static final int STRANGE_BUSH_GREEN = 12150;
+    public static final int FLOWERING_BUSH_BLUE = 12151;
+    public static final int STRANGE_BUSH_BLUE = 12152;
 
 }
